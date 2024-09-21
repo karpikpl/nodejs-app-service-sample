0baebedb612a5703b779f4d1db3fc2efe532176f
Piotr Karpala <piotrkarpala@microsoft.com>
2024-09-20 21:24:27 -0400

    init commit

---
+++ src/app2/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+
+    const users = [
+        { name: 'John', age: 30 },
+        { name: 'Jane', age: 25 },
+        { name: 'Jim', age: 27 }
+    ]
+    res.send(users);
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ src/myApp1/routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+
+  const url = process.env.API_URL || 'http://localhost:3000';
+
+  try {
+    const response = await axios.get(`${url}/users`);
+    // get users from another service
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
